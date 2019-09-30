d391542ce23c25d659461c7e7d1ba64d2667cb44
Juan Carlos Labrandero <jcharlylc@gmail.com>
2019-09-30 11:45:16 -0400

    First basic types

---
+++ include/system/types.hpp
@@ -0,0 +1,23 @@
+
+/****************************************************************************************
+
+	Spiga Engine.
+	Copyright (c) 2012-2015, Juan Carlos Labrandero.
+	For conditions of distribution and use, see copyright notice in license.txt
+
+****************************************************************************************/
+
+#ifndef __spiga_types_hpp__
+#define __spiga_types_hpp__
+
+namespace spi {
+
+typedef unsigned char uchar;
+typedef unsigned char ubyte;
+typedef unsigned short ushort;
+typedef unsigned int uint;
+typedef unsigned long ulong;
+
+} //end spi namespace
+
+#endif //__spiga_types_hpp__
